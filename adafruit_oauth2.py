1278422b85623ae833f35499e9969376798f7c46
brentru <robots199@me.com>
2021-01-06 12:28:57 -0500

    add request codes, example

--- adafruit_oauth2.py
+++ adafruit_oauth2.py
@@ -1,4 +1,3 @@
-# SPDX-FileCopyrightText: 2017 Scott Shawcroft, written for Adafruit Industries
 # SPDX-FileCopyrightText: Copyright (c) 2021 Brent Rubell for Adafruit Industries
 #
 # SPDX-License-Identifier: MIT
@@ -16,21 +15,70 @@ Implementation Notes
 
 **Hardware:**
 
-.. todo:: Add links to any specific hardware product page(s), or category page(s). Use unordered list & hyperlink rST
-   inline format: "* `Link Text <url>`_"
 
 **Software and Dependencies:**
 
 * Adafruit CircuitPython firmware for the supported boards:
   https://github.com/adafruit/circuitpython/releases
 
-.. todo:: Uncomment or remove the Bus Device and/or the Register library dependencies based on the library's use of either.
-
-# * Adafruit's Bus Device library: https://github.com/adafruit/Adafruit_CircuitPython_BusDevice
-# * Adafruit's Register library: https://github.com/adafruit/Adafruit_CircuitPython_Register
 """
 
 # imports
 
 __version__ = "0.0.0-auto.0"
 __repo__ = "https://github.com/adafruit/Adafruit_CircuitPython_OAuth2.git"
+
+# Google's authorization server
+DEVICE_AUTHORIZATION_ENDPOINT = "https://oauth2.googleapis.com/device/code"
+
+class OAuth2:
+    """Implements OAuth2.0 authorization to access
+    Google APIs via the OAuth 2.0 limited-input device application flow.
+    https://developers.google.com/identity/protocols/oauth2/limited-input-device
+
+    :param requests: An adafruit_requests object.
+    :param str client_id: The client ID for your application.
+    :param str client_secret: The client secret obtained from the API Console.
+    :param list scopes: Scopes that identify the resources the application
+                        can access on the user's behalf.
+
+    """
+    def __init__(self, requests, client_id, client_secret, scopes):
+        self._requests = requests
+        self._client_id = client_id
+        self._client_secret = client_secret
+        self._scopes = scopes
+        # empty properties for tokens during auth. process
+        self._device_code = None
+        # length of time that the codes above are valid, in seconds
+        self._expiration_time = None
+        # length of time we'll wait between polling the auth. server
+        self._interval = None
+        # url user must navigate to on a browser
+        self._verification_url = None
+        # identifies the scopes requested by the application
+        self._user_code = None
+
+    def request_codes(self):
+        """Identifies your application and access scopes with Google's
+        authorization server. Attempts to request device and user codes 
+        """
+        headers = {"Host": "oauth2.googleapis.com",
+                   "Content-Type": "application/x-www-form-urlencoded",
+                   "Content-Length":"0"}
+        url = DEVICE_AUTHORIZATION_ENDPOINT + \
+              "?client_id={0}&scope={1}".format(self._client_id, self._scopes)
+
+        response = self._requests.post(url, headers=headers)
+        json_resp = response.json()
+        response.close()
+        # Handle `quota exceeded` error
+        if 'error_code' in json_resp:
+            raise RuntimeError('Error, quota exceeded: ', json_resp['error_code'])
+
+        # parse response
+        self._device_code = json_resp['device_code']
+        self._expiration_time = json_resp['expires_in']
+        self._interval = json_resp['interval']
+        self._verification_url = json_resp['verification_url']
+        self._user_code = json_resp['user_code']
--- examples/oauth2_simpletest.py
+++ examples/oauth2_simpletest.py
@@ -1,3 +1,44 @@
-# SPDX-FileCopyrightText: 2017 Scott Shawcroft, written for Adafruit Industries
+# SPDX-FileCopyrightText: 2020 Brent Rubell, written for Adafruit Industries
 #
 # SPDX-License-Identifier: Unlicense
+import board
+import busio
+from digitalio import DigitalInOut
+import adafruit_esp32spi.adafruit_esp32spi_socket as socket
+from adafruit_esp32spi import adafruit_esp32spi
+import adafruit_requests as requests
+import adafruit_oauth2 as oauth2
+
+# Add a secrets.py to your filesystem that has a dictionary called secrets with "ssid" and
+# "password" keys with your WiFi credentials. DO NOT share that file or commit it into Git or other
+# source control.
+# pylint: disable=no-name-in-module,wrong-import-order
+try:
+    from secrets import secrets
+except ImportError:
+    print("WiFi secrets are kept in secrets.py, please add them there!")
+    raise
+
+esp32_cs = DigitalInOut(board.ESP_CS)
+esp32_ready = DigitalInOut(board.ESP_BUSY)
+esp32_reset = DigitalInOut(board.ESP_RESET)
+
+spi = busio.SPI(board.SCK, board.MOSI, board.MISO)
+esp = adafruit_esp32spi.ESP_SPIcontrol(spi, esp32_cs, esp32_ready, esp32_reset)
+
+print("Connecting to AP...")
+while not esp.is_connected:
+    try:
+        esp.connect_AP(secrets["ssid"], secrets["password"])
+    except RuntimeError as e:
+        print("could not connect to AP, retrying: ", e)
+        continue
+print("Connected to", str(esp.ssid, "utf-8"), "\tRSSI:", esp.rssi)
+
+# Initialize a requests object with a socket and esp32spi interface
+socket.set_interface(esp)
+requests.set_socket(socket, esp)
+
+google_auth = oauth2(requests, client_id, client_secret, scopes)
+
+google_auth.request_codes()
